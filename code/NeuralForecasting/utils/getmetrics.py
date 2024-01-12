a5266f30b0f8261505a8638249d4a6a9d96e2dce
Mohammed <maldosari@outlook.sa>
2024-01-12 17:26:35 -0500

    Update getmetrics.py

--- code/NeuralForecasting/utils/getmetrics.py
+++ code/NeuralForecasting/utils/getmetrics.py
@@ -1,7 +1,21 @@
-def getmetrics(main_output, actual, forecasts):
-    print("Normalized test {}".format(main_output))
-    print('N:{}, MSE:{}, MAE:{}, sMAPE:{}'.format(len(actual),
-                                                  ('%.3f' %mse(actual, forecasts)),
-                                                  ('%.3f' %mae(actual, forecasts)),
-                                                  ('%.3f' %smape(actual, forecasts))))
-    return
+def getmetrics(main_output: str, actual: np.array, forecasts: np.array) -> (float, float, float):
+    """
+    A function used for returning the MSE, MAE, sMAPE metrics.
+
+    Arguments
+    ----------
+    main_output: str
+        the name of the main output feature for evaluation
+    actual: np.array
+        the observed test set values aligned with the forecasts
+    forecasts: np.array
+        the output from a forecasting model, i.e. RandomWalk
+           
+    Returned Values
+    ----------
+    mse: float
+    mae: float
+    smape: float
+
+    """
+    return ('%.3f' %mse(actual, forecasts)), ('%.3f' %mae(actual, forecasts)), ('%.3f' %smape(actual, forecasts))
