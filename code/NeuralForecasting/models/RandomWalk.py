75096c2875d0e0d3d21db3798935b0934b478208
Mohammed <maldosari@outlook.sa>
2024-01-12 19:21:19 -0500

    Update RandomWalk.py

--- code/NeuralForecasting/models/RandomWalk.py
+++ code/NeuralForecasting/models/RandomWalk.py
@@ -1,14 +1,36 @@
-from data.data_loading import load_data
-from data.data_loading import plot_train_test
+import numpy as np
+from typing import List, Union
+from data.data_loading import load_data, plot_train_test
 from data.data_transforms import data_transform_std
 from data.data_splitting import train_test_split
-import numpy as np
 from utils.metrics import mse, mae, smape
-from typing import List
-from typing import Union
 from utils.getmetrics import getmetrics
 
 def RandomWalk(file_name: str, training_ratio: float, horizon: int, main_output: str, normalization: bool) -> None:
+    """
+    A function used for producing forecasts based on the Random Walk model that simply projects a current value into the future (yhat[t] = y[t-h]).
+    
+    Arguments
+    ----------
+    file_name: str
+        the file path for csv data file.
+    training_ratio: float
+        the training ratio used for splitting the dataset into train and test
+    horizon: int
+        how many time steps ahead to make the forecasts
+    main_output: str
+        the main output column/feature, e.g. '% WEIGHTED ILI'
+    normalization: bool
+        specifies whether the data is normalized or original
+        
+    Returned Values
+    ----------
+    mse: float
+    mae: float
+    smape: float
+
+    """
+    
     data = load_data(file_name, main_output = main_output)
     train_size = int(training_ratio*len(data))
     if normalization:
@@ -20,14 +42,12 @@ def RandomWalk(file_name: str, training_ratio: float, horizon: int, main_output:
     actual = data[[main_output]]                                # Actual complete dataset for main output.
     forecasts = []
     for i in range(len(test_data_MO) - horizon):
-        forecasts.append(actual.iloc[train_size + i - 1,:])     # For Random Walk, start with last value from tarining set.
+        forecasts.append(actual.iloc[train_size + i - 1,:])     # For Random Walk, start with the last value from the training set.
 
-    actual = actual[train_size + horizon:]                      # Aligning actual with forecasts so both have same length.
+    actual = actual[train_size + horizon:]                      # Aligning actual with forecasts so both have the same length.
     actual = np.array(actual)                                   # Convert actual a numpy array
 
     plot_train_test(data, main_output, train_size, train_data_MO, test_data_MO, forecasts)
     mse, mae, smape = getmetrics(actual, forecasts)
     
-    print("Test set metrics {}".format(main_output))
-    print('N:{}, MSE:{}, MAE:{}, sMAPE:{}'.format(len(actual), mse, mae, smape))
-    return
+    return len(actual), mse, mae, smape
